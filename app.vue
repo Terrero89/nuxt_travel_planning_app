08234ca3d3667759a037c551be007f0a65e1ed11
Terrero89 <sergio-terrero@hotmail.com>
2024-08-26 07:59:36 -0400

    setup started

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+ <UAccordion class="acc"
+     open-icon="i-heroicons-plus"
+    close-icon="i-heroicons-minus"
+    :items="[{ label: '1. What is Nuxt UI?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: '2. Getting Started', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: '3. Theming', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: '4. Components', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }]"
+  />
   </div>
 </template>
+
+<style scoped>
+
+
+</style>
